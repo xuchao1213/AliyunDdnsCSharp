5779da66e09295fd5e5344de0e5eb6511090a764
XuChao <xuchao_1213@163.com>
2019-10-08 22:58:36 +0800

    fix https://github.com/xuchao1213/AliyunDdnsCSharp/issues/2

--- AliyunDdnsCSharp/Utils/HttpUtils.cs
+++ AliyunDdnsCSharp/Utils/HttpUtils.cs
@@ -23,7 +23,9 @@ namespace AliyunDdnsCSharp.Utils
         private static readonly HttpClientHandler GHttpHandler = new HttpClientHandler
         {
             UseProxy = false,
-            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+            //UseCookies=false,
+            AllowAutoRedirect=false,
         };
         private static readonly HttpClient GlobalClient = new HttpClient(GHttpHandler)
         {
@@ -34,6 +36,8 @@ namespace AliyunDdnsCSharp.Utils
         {
             UseHttps();
             GlobalClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("GZIP"));
+            //fix https://github.com/xuchao1213/AliyunDdnsCSharp/issues/2
+            GlobalClient.DefaultRequestHeaders.UserAgent.TryParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36");
         }
         private static void UseHttps()
         {
