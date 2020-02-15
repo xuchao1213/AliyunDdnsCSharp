8fd8553fd8310be907b68431043c14df7e3bc263
XuChao <xuchao_1213@163.com>
2020-02-15 18:44:51 +0800

    :zap: 优化获取本地IPv6地址的逻辑，尽量不获取临时地址

--- AliyunDdnsCSharp/Utils/NetWorkUtils.cs
+++ AliyunDdnsCSharp/Utils/NetWorkUtils.cs
@@ -1,29 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Text;
 
 namespace AliyunDdnsCSharp.Utils
 {
-   public static class NetWorkUtils
+    public static class NetWorkUtils
     {
         public static string GetLocalIpV6Address()
         {
-            string ret = "";
-            string name = Dns.GetHostName();
-            IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
-            foreach (IPAddress ipa in ipadrlist)
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            if (adapters.Length == 0) { return ""; }
+            foreach (NetworkInterface adapter in adapters)
             {
-                if (ipa.AddressFamily == AddressFamily.InterNetworkV6)
+                if (adapter.NetworkInterfaceType!=NetworkInterfaceType.Ethernet&&
+                    adapter.NetworkInterfaceType != NetworkInterfaceType.Wireless80211 &&
+                    adapter.NetworkInterfaceType != NetworkInterfaceType.GigabitEthernet
+                    )
                 {
-                    if (ipa.IsIPv6LinkLocal || ipa.IsIPv6SiteLocal ||ipa.IsIPv6Multicast) { continue; }
-                    ret = ipa.ToString();
-                    break;
+                    continue;
+                }
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                UnicastIPAddressInformationCollection uniCast = adapterProperties.UnicastAddresses;
+                if (uniCast.Count > 0)
+                {
+                    foreach (UnicastIPAddressInformation uni in uniCast)
+                    {
+                        var ipa = uni.Address;
+                        //过滤非IPV6地址
+                        if (ipa.AddressFamily != AddressFamily.InterNetworkV6){
+                            continue;
+                        }
+                        if (uni.DuplicateAddressDetectionState!= DuplicateAddressDetectionState.Preferred)
+                        {
+                            continue;
+                        }
+                        if (ipa.IsIPv6LinkLocal || ipa.IsIPv6SiteLocal || ipa.IsIPv6Multicast) { continue; }
+                        //优先取Dhcp分配的
+                        if (uni.PrefixOrigin== PrefixOrigin.Dhcp && uni.SuffixOrigin==SuffixOrigin.OriginDhcp)
+                        {
+                            return ipa.ToString();
+                        }
+                        //其次取RA & LinkLayerAddress
+                        if (uni.PrefixOrigin == PrefixOrigin.RouterAdvertisement && uni.SuffixOrigin == SuffixOrigin.LinkLayerAddress)
+                        {
+                            return ipa.ToString();
+                        }
+                        //最后取RA & Random
+                        if (uni.PrefixOrigin == PrefixOrigin.RouterAdvertisement && uni.SuffixOrigin == SuffixOrigin.Random)
+                        {
+                            return ipa.ToString();
+                        }
+                    }
                 }
             }
-            return ret;
+            return "";
+        //    string ret = "";
+        //    string name = Dns.GetHostName();
+        //    IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
+        //    foreach (IPAddress ipa in ipadrlist)
+        //    {
+        //        if (ipa.AddressFamily == AddressFamily.InterNetworkV6)
+        //        {
+        //            if (ipa.IsIPv6LinkLocal || ipa.IsIPv6SiteLocal ||ipa.IsIPv6Multicast) { continue; }
+        //            ret = ipa.ToString();
+        //            break;
+        //        }
+        //    }
+        //    return ret;
         }
     }
 }
